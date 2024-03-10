0a3e8d1c0c340aed50bff019b6f5a94690fb45dc
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-03-10 19:24:00 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,14 @@
+/* Adding Comments: Choose two of the programs you have written, and add atleast one
+comment to each.If you don't have anything specific to write because your programs are too
+ simple at this point, just add your name and the current date at the top of each program
+ file. Then, write one sentence describing what the program does.*/
+var a = 20;
+var b = 18;
+//Addition
+console.log("".concat(a, "+").concat(b, "=").concat(a + b));
+//Subtraction
+console.log("".concat(a, "-").concat(b, "=").concat(a - b));
+//Multiplication
+console.log("".concat(a, "*").concat(b, "=").concat(a * b));
+//Division
+console.log("".concat(a, "/").concat(b, "=").concat(a / b));
